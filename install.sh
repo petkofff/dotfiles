d3478afcfdb21c1b725482de6e5a821885de9feb
petkofff <gppetkoff@abv.bg>
2018-07-13 21:38:45 +0300

    install.sh updated to create symlinks to the files from the repo, .aliases-and-functions moved

--- install.sh
+++ install.sh
@@ -1,13 +1,29 @@
 #!/bin/bash
 
-pathToAliasesAndFunctions="~/.aliases-and-functions"
+# Usage:
+# ``` 
+# cd <path to the repo>
+# bash install.sh
+# ```
 
-cp ./bash/.aliases-and-functions ~
+aliasesAndFunctions=".aliases-and-functions"
+files="$aliasesAndFunctions .tmux.conf .gitconfig"
+backupDir=~/.dotfilesbackup
+currentDir=`pwd`
+
+mkdir -p $backupDir
+
+for file in $files; do
+    mv ~/$file $backupDir               # backup old config files
+    ln -fs $currentDir/$file ~/$file    # create symlinks to the files from the repo
+done
+
+pathToAliasesAndFunctions="~/$aliasesAndFunctions"
+
+# source .aliases-and-functions in .bashrc if it hasn't been done
 if grep -q "source $pathToAliasesAndFunctions" ~/.bashrc; then
     echo "$pathToAliasesAndFunctions already sourced"
     else
         printf "\nsource $pathToAliasesAndFunctions\n" >> ~/.bashrc
 fi
 
-cp .tmux.conf ~
-cp .gitconfig ~
