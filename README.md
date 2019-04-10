0bfb545bcda58ce18ed86e78638fda5babcd412d
petkofff <gppetkoff@abv.bg>
2019-04-11 02:37:03 +0300

    preinstall.sh functionality moved to manage.sh, added options support to manage.sh, updated README.md

--- README.md
+++ README.md
@@ -2,10 +2,9 @@
 My dotfiles
 
 ## Install
-`preinstall.sh` installs some useful packages, `install.sh` backs up existing config files and creates symlinks to the files from the repo.
+`manage.sh` backs up existing config files and creates symlinks to the files from the repo.
 ```
 git clone https://github.com/petkofff/dotfiles.git
 cd dotfiles
-bash preinstall.sh all
-bash install.sh
+bash manage.sh
 ```
--- manage.sh
+++ manage.sh
@@ -3,15 +3,54 @@
 # Usage:
 # ```
 # cd <path to the repo>
-# bash install.sh
+# bash manage.sh
 # ```
+
+ARGS=$@
 
 aliasesAndFunctions=".aliases-and-functions"
 backupDir=~/.dotfilesbackup
 currentDir=$(pwd)
 
+function help_message {
+    # TODO
+}
 
-function unignored-files {
+function restore_backup {
+    # TODO
+}
+
+function passed {
+    for arg in $ARGS; do
+       if [ "$1" = "$arg" ]; then
+            return 0
+        fi
+    done
+
+    return 1
+}
+
+packages=""
+
+if passed "base"; then
+    packages="$(cat packages/arch/base.txt)"
+fi
+
+if passed "all"; then
+    packages="$(cat packages/arch/all.txt)"
+fi
+
+if [ ! "$packages" = "" ]; then
+    sudo pacman -S --noconfirm --needed $packages
+fi
+
+passed "-spacemacs" || passed "-space" || {
+    if [ ! -f ~/.emacs.d/spacemacs.mk ]; then
+        git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d
+    fi
+}
+
+function unignored_files {
     set -f
 
     find . -type f  $(printf "! -wholename ./%s " \
@@ -19,41 +58,50 @@ function unignored-files {
         -printf '%P\n'
 
     set +f
-
-    # TODO: implement without turning off expansion
 }
 
-# files to install
-files="$(unignored-files)"
+function make_symlinks_and_backup {
+    # files to install
+    files="$(unignored_files)"
 
-echo "Processing:"
-for file in $files; do
-    echo "$file"
+    echo "Processing:"
+    for file in $files; do
+        echo "$file"
 
-    parentDir=$(dirname $file)
+        parentDir=$(dirname $file)
 
-    # ensure that parent directories exist
-    mkdir -p $backupDir/$parentDir ~/$parentDir
+        # ensure that parent directories exist
+        mkdir -p $backupDir/$parentDir ~/$parentDir
 
-    # backup old config files
-    mv ~/$file $backupDir/$parentDir
+        # backup old config files
+        mv ~/$file $backupDir/$parentDir
+        #
+        # TODO: don't backup already installed symlinks to the
+        #       files from the repo
+        #
 
-    # create symlinks to the files from the repo
-    ln -fs $currentDir/$file ~/$file
-done
+        # create symlinks to the files from the repo
+        ln -fs $currentDir/$file ~/$file
+    done
 
-echo
+    echo
 
-find .scripts -type f -exec chmod +x {} \;
+    find .scripts -type f -exec chmod +x {} \;
 
-pathToAliasesAndFunctions="~/$aliasesAndFunctions"
+    path_to_aliases_and_functions="~/$aliasesAndFunctions"
 
-# source .aliases-and-functions in .bashrc if it hasn't been done
-if grep -q "source $pathToAliasesAndFunctions" ~/.bashrc; then
-    echo "$pathToAliasesAndFunctions already sourced"
-else
-    printf "\nsource $pathToAliasesAndFunctions\n" >>~/.bashrc
-fi
+    # source .aliases-and-functions in .bashrc if it hasn't been done
+    if grep -q "source $path_to_aliases_and_functions" ~/.bashrc; then
+        echo "$path_to_aliases_and_functions already sourced"
+    else
+        printf "\nsource $path_to_aliases_and_functions\n" >>~/.bashrc
+    fi
+}
+
+
+passed "-symlinks" || passed "-links" || passed "-symlink" || passed "-link" || {
+    make_symlinks_and_backup
+}
 
 pathToPlug=~/.vim/autoload/plug.vim
 
